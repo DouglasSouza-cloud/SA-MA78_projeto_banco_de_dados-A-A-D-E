a4f1100c5421485a651969c30a81441e39ae18b1
DOUGLAS DE SOUZA PAIXÃO <douglas_paixao@estudante.sesisenai.org.br>
2026-07-08 20:59:44 -0300

    feat: Adicionando conexão com mysql

---
+++ src/colaborador.py
@@ -0,0 +1,20 @@
+from database import conectar
+
+def listar_colaborador():
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    sql = """
+        SELECT
+            c.nome_colaborador,
+            c.cpf_colaborador
+        FROM colaborador c
+    """
+
+    cursor.execute(sql)
+    dados = cursor.fetchall()
+
+    cursor.close()
+    conexao.close()
+
+    return dados
