f4bc848ae5f7e0ba78d74078cbefee812cf953d5
AUGUSTO STERNADT DZIEDICZ <augusto_dziedicz@estudante.sesisenai.org.br>
2026-07-22 20:08:32 -0300

    feat: Adicionando roles

--- script/05-users.sql
+++ script/05-users.sql
@@ -1,10 +1,10 @@
 use INOVE_CONTABILIDADE;
 
 # Criação de usuários
-create USER 'douglas'@'%' IDENTIFIED by 'douglas123';
-create USER 'amanda'@'%' IDENTIFIED by 'amanda123';
-create USER 'emanuel'@'%' IDENTIFIED by 'emanuel123';
-create USER 'augusto'@'%' IDENTIFIED by 'augusto123';
+CREATE USER 'douglas'@'%' IDENTIFIED by 'douglas123';
+CREATE USER 'amanda'@'%' IDENTIFIED by 'amanda123';
+CREATE USER 'emanuel'@'%' IDENTIFIED by 'emanuel123';
+CREATE USER 'augusto'@'%' IDENTIFIED by 'augusto123';
 
 #Dando privilégios
 GRANT all ON INOVE_CONTABILIDADE.* TO 'douglas'@'%';
@@ -22,3 +22,35 @@ SHOW GRANTS FOR 'douglas'@'%';
 SHOW GRANTS FOR 'amanda'@'%';
 SHOW GRANTS FOR 'emanuel'@'%';
 SHOW GRANTS FOR 'augusto'@'%';
+
+
+#Criação de cargos
+
+CREATE ROLE 'DBA'; -- DBA é o Database Administrador
+
+CREATE ROLE 'Editor';
+
+CREATE ROLE 'Leitor';
+
+CREATE ROLE 'Desenvolvedor';
+
+
+#Dando os privelégios para os cargos
+
+GRANT all ON INOVE_CONTABILIDADE.* TO 'DBA';
+
+GRANT SELECT, INSERT, UPDATE ON INOVE_CONTABILIDADE.* TO 'Editor';
+
+GRANT SELECT ON INOVE_CONTABILIDADE.* TO 'Leitor';
+
+GRANT SELECT ON INOVE_CONTABILIDADE.* TO 'Desenvolvedor';
+
+#Mostrando os privilégios
+
+SHOW GRANTS FOR 'DBA';
+
+SHOW GRANTS FOR 'Editor';
+
+SHOW GRANTS FOR 'Leitor';
+
+SHOW GRANTS FOR 'Desenvolvedor';
